c172b856504db32b5ff7c9144bd064204e635f3c
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-11-12 20:16:34 -0300

    feat: modulo de login criado

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaService } from '../prisma/prisma.service';
+import "dotenv/config"
 
 @Module({
+  imports: [
+    JwtModule.register({
+    global: true,
+    secret: process.env.JWT_SECRET,
+    signOptions: {},
+  })],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, PrismaService],
 })
 export class AuthModule {}
