58ea956cf0a61c359727bd46a7ffbea04d1b9658
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-11-12 20:16:17 -0300

    feat: serviço de login criado

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,83 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { LoginAuthDto } from './dto/login-auth.dto';
+import * as bcrypt from "bcrypt"
+import { PrismaService } from '../prisma/prisma.service';
+import { UsuarioService } from '../usuario/usuario.service';
+import { PrismaClientKnownRequestError } from '../../generated/internal/prismaNamespace';
+import { Usuario } from '../../generated/browser';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    private readonly prisma: PrismaService,
+    private jwtService: JwtService
+  ) { }
+
+  async cadastrar(createAuthDto: CreateAuthDto) {
+    const { senha, username, nome, bairro, cep, cidade, estado, logradouro, numero, complemento } = createAuthDto
+    try {
+      const hashPassword = await bcrypt.hash(senha, 10);
+      const user = await this.prisma.usuario.create({
+        data: {
+          username,
+          senha: hashPassword,
+          nome: nome.toUpperCase(),
+          bairro: bairro.toUpperCase(),
+          cep: cep.toUpperCase(),
+          cidade: cidade.toUpperCase(),
+          estado: estado.toUpperCase(),
+          logradouro: logradouro.toUpperCase(),
+          numero: numero.toUpperCase(),
+          complemento: complemento ? complemento.toUpperCase() : null,
+        }
+      })
+      const {senha: sen, ...rest} = user
+
+      return rest;      
+
+    } catch (e) {
+      if (e instanceof HttpException) throw e
+      if (e instanceof PrismaClientKnownRequestError) {
+        if (e.code === 'P2002') {
+          throw new ConflictException(`O username já está em uso.`);
+        }
+      }
+      throw new HttpException("Erro no servidor", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  async logar(loginAuthDto: LoginAuthDto) {
+    const {username, senha} = loginAuthDto;
+    try {
+      const user = await this.prisma.usuario.findUnique({
+        where: { username },
+      });
+      if (!user) {
+        throw new HttpException("Usuário não encontrado.", HttpStatus.UNAUTHORIZED);
+      }
+
+      const isPasswordValid = await bcrypt.compare(senha, user.senha);
+
+      if (!isPasswordValid) {
+        throw new HttpException("Senha incorreta", HttpStatus.UNAUTHORIZED);
+      }
+
+      const dadosUsuario: Partial<Usuario>= {...user}
+      delete dadosUsuario.senha
+      
+      return {
+        access_token: await this.jwtService.signAsync(dadosUsuario)
+      }
+
+
+    } catch (e) {
+      if(e instanceof HttpException) throw e;
+
+      console.error("Erro enquanto tentava se logar: ", e)
+      throw new HttpException("Erro no servidor", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
 }
