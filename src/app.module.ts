8d923bdbe63a4f652e7a82d61db25ba9e2d3ecf7
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-11-13 16:52:27 -0300

    feat: modulos adicionado ao app

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,15 @@ import { PrismaService } from './prisma/prisma.service';
 import { UsuarioModule } from './usuario/usuario.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { CategoriaModule } from './categoria/categoria.module';
+import { AnuncioModule } from './anuncio/anuncio.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    UsuarioModule, AuthModule],
+    UsuarioModule, AuthModule, CategoriaModule, AnuncioModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
