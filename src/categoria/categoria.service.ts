e6c5e6c6ba1d8f79a817c957fd770632f09c34f8
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-11-13 19:06:46 -0300

    feat: seed de categorias criado

--- src/categoria/categoria.service.ts
+++ src/categoria/categoria.service.ts
@@ -1,75 +1,90 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CreateCategoriaDto } from './dto/create-categoria.dto';
 import { UpdateCategoriaDto } from './dto/update-categoria.dto';
 import { PrismaService } from '../prisma/prisma.service';
+import { Categoria } from '../../generated/client';
 
 @Injectable()
 export class CategoriaService {
   constructor (private readonly prisma: PrismaService){}
 
-  private readonly defaultCategories = [
-    // Serviços
-    'SERVIÇOS DOMÉSTICOS',
-    'REPARAÇÃO | CONSERTO | REFORMA',
-    'SERVIÇOS DE TI',
-    'AULAS E CURSOS',
-    'CONSULTORIA',
-    'EVENTOS',
-    'SAÚDE | BELEZA | BEM-ESTAR',
-    'TRANSPORTE | MUDANÇAS',
-    'TURISMO',
-    'PROFISSIONAIS LIBERAIS',
-    'TRADUÇÃO',
-    'OUTROS',
-
-    // Produtos
-    'IMOVEIS',
-    'AUTOS',
-    'AUTOPEÇAS',
-    'CASA | DECORAÇÃO | UTENSÍLIOS',
-    'MÓVEIS',
-    'ELETRO',
-    'MATERIAIS DE CONSTRUÇÃO',
-    'CELULARES | TELEFONIA',
-    'INFORMÁTICA',
-    'GAMES',
-    'TVs | VÍDEO',
-    'ÁUDIO',
-    'CÂMERAS | DRONES',
-    'MODA | BELEZA',
-    'COMÉRCIO',
-    'ESCRITÓRIO',
-    'MÚSICA | HOBBIES',
-    'ESPORTES | FITNESS',
-    'ARTIGOS INFANTIS',
-    'ANIMAIS DE ESTIMAÇÃO',
-    'AGRO E INDUSTRIA'
-  ];
+  private readonly logger = new Logger(CategoriaService.name);
 
   async onModuleInit(){
+    const categoriaServicos = [
+      // Serviços
+      'SERVIÇOS DOMÉSTICOS',
+      'REPARAÇÃO | CONSERTO | REFORMA',
+      'SERVIÇOS DE TI',
+      'AULAS E CURSOS',
+      'CONSULTORIA',
+      'EVENTOS',
+      'SAÚDE | BELEZA | BEM-ESTAR',
+      'TRANSPORTE | MUDANÇAS',
+      'TURISMO',
+      'PROFISSIONAIS LIBERAIS',
+      'TRADUÇÃO',
+      'OUTROS',
+    ]
+  
+    const categoriaProdutos = [
+      // Produtos
+      'IMOVEIS',
+      'AUTOS',
+      'AUTOPEÇAS',
+      'CASA | DECORAÇÃO | UTENSÍLIOS',
+      'MÓVEIS',
+      'ELETRO',
+      'MATERIAIS DE CONSTRUÇÃO',
+      'CELULARES | TELEFONIA',
+      'INFORMÁTICA',
+      'GAMES',
+      'TVs | VÍDEO',
+      'ÁUDIO',
+      'CÂMERAS | DRONES',
+      'MODA | BELEZA',
+      'COMÉRCIO',
+      'ESCRITÓRIO',
+      'MÚSICA | HOBBIES',
+      'ESPORTES | FITNESS',
+      'ARTIGOS INFANTIS',
+      'ANIMAIS DE ESTIMAÇÃO',
+      'AGRO E INDUSTRIA'
+    ];
+  
     try {
       const count = await this.prisma.categoria.count();
-      if (count > 0) {
-        console.log('Categorias já populadas. Seed ignorado.');
+      if (count ===  33) {
+        this.logger.log('Categorias já populadas. Seed ignorado.');
         return;
       }
 
-      console.warn('Nenhuma categoria encontrada. Populando o banco...');
+      this.logger.warn('Nenhuma categoria encontrada. Populando o banco...');
 
-      const dataToCreate = this.defaultCategories.map((nome) => ({
+      const categoriasParaCriar: Omit<Categoria, "id">[] = []
+      categoriaProdutos.forEach((nome) => (
+        categoriasParaCriar.push({
         nome: nome,
-      }));
+        tipo: "PRODUTO"
+      })));
+
+      categoriaServicos.forEach((nome) => (
+        categoriasParaCriar.push({
+        nome: nome,
+        tipo: "SERVICO"
+      })));
       
-      // 7. Usa createMany para inserir todas de uma vez (eficiente)
+      
+
       const result = await this.prisma.categoria.createMany({
-        data: dataToCreate,
-        skipDuplicates: true, // Garante que não vai quebrar se algo já existir
+        data: categoriasParaCriar,
+        skipDuplicates: true,
       });
 
-      console.log(`${result.count} categorias padrão criadas com sucesso.`);
+      this.logger.log(`${result.count} categorias padrão criadas com sucesso.`);
     
     } catch (error) {
-      console.error('Falha ao popular categorias padrão:', error);
+      this.logger.error('Falha ao popular categorias padrão:', error);
     }
   }
 
