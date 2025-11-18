abbce990ef1c73c4a6b38a30259705ad25eeff15
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-11-18 15:12:39 -0300

    feat: servidor de arquivos configurado

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,18 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { CategoriaModule } from './categoria/categoria.module';
 import { AnuncioModule } from './anuncio/anuncio.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "../..", 'uploads'),
+      serveRoot: '/uploads',
     }),
     UsuarioModule, AuthModule, CategoriaModule, AnuncioModule],
   controllers: [AppController],
